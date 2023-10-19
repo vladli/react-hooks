326bb8c61cf7b03c3e0eba62b03ee303f7260924
Vladislav Li <vladislav.li911@gmail.com>
2023-10-19 22:38:10 +0900

    first commit

---
+++ useIsMobile.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(
+        window.innerWidth <= 768 ||
+          /iPhone|iPad|iPod|Android|webOS|BlackBerry|Windows Phone/i.test(
+            navigator.userAgent
+          )
+      );
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return isMobile;
+};
+
+export default useIsMobile;
